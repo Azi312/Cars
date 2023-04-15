a43c76d09679f8e69858a8a5e960537cafc6a6b4
Azi312 <aziz260110@gmail.com>
2023-04-15 23:37:12 +0600

    last commit

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
 	apiKey: 'AIzaSyCZ6Yf2jjb7J7cjgIpibxf4altukhm8w8Y',
 	authDomain: 'cars-95559.firebaseapp.com',
@@ -14,15 +10,7 @@ const firebaseConfig = {
 	appId: '1:542451743400:web:b682e8809564f239a6a09d',
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 
 export default app
-
-// apiKey: process.env.FIREBASE_API_KEY,
-// 	authDomain:process.env.FIREBASE_AUTH_DOMAIN,
-// 	projectId:process.env.FIREBASE_PROJECT_ID,
-// 	storageBucket:process.env.FIREBASE_STORAGE_BUSKET,
-// 	messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID,
-// 	appId:process.env.FIREBASE_API_ID,
